c85421fd0134c6f4e88100b38e13a2db68b16c83
Irina <iargina.irina@gmail.com>
2022-11-14 23:23:02 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
+const form = document.querySelector(`.form`);
+const inputAmount = document.querySelector(`[name="amount"]`);
+const inputStep = document.querySelector(`[name="step"]`);
+const inputDelay = document.querySelector(`[name="delay"]`);
+
+form.addEventListener(`submit`, creation);
+
+function creation() {
+  const amount = Number(inputAmount.value);
+  const delayFirst = Number(inputDelay.value);
+  const step = Number(inputStep.value);
+
+  for (let i = 1; i <= amount; i++) {
+    let position = i;
+    let delay = delayFirst + step * i;
+    createPromise(position, delay);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }

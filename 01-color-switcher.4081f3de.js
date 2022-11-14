74037f42f70d4c327fec70055b50d5739cd59ea7
iargina <113432623+iargina@users.noreply.github.com>
2022-11-14 11:10:55 +0000

    Deploying to gh-pages from @ iargina/goit-js-hw-09@f03ce52340c51cc328878b52ff5e5f72ce05aa88 🚀

--- 01-color-switcher.4081f3de.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]");console.log(t);
-//# sourceMappingURL=01-color-switcher.4081f3de.js.map
--- 01-color-switcher.6028e4a0.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]");console.log(t)}();
-//# sourceMappingURL=01-color-switcher.6028e4a0.js.map

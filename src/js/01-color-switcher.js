f03ce52340c51cc328878b52ff5e5f72ce05aa88
Irina <iargina.irina@gmail.com>
2022-11-14 13:10:35 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,28 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-const start = document.querySelector(`button[data-start]`)
-console.log(start);
+const start = document.querySelector(`button[data-start]`);
+const stop = document.querySelector(`button[data-stop]`);
+
+start.addEventListener(`click`, changeCollor);
+
+function changeCollor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorChange = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+  start.setAttribute('disabled', 'disabled');
+}
+
+stop.addEventListener(`click`, standartStyle);
+
+function standartStyle() {
+  document.body.removeAttribute(`style`);
+  start.removeAttribute('disabled');
+  clearInterval(colorChange);
+}
+
+console.log(document.body);

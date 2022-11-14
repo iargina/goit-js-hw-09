d118a25e3b19491cd97d21aa50b6336b0a99bfe8
Irina <iargina.irina@gmail.com>
2022-11-14 12:58:39 +0200

    json

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,6 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const start = document.querySelector(`button[data-start]`)
+console.log(start);
